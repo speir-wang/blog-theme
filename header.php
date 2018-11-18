5782e2c652fa3859f642e55487acb56aefe09459
He Wang <speir.wang@gmail.com>
2018-11-18 11:56:42 +1100

    add google tag mananger

--- header.php
+++ header.php
@@ -13,10 +13,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="google-site-verification" content="xwxO355DgNIGHoX1Fwz3lnNPXqqPEoCa_eig5JYes_k" />
 <?php wp_head(); ?>
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TZFJFF8');</script>
+<!-- End Google Tag Manager -->
 </head>
 
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZFJFF8"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <div id="page" class="site">
 
